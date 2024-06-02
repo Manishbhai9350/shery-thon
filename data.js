85e1605ba0da7bf77e215a1bdd8ea399f873c56a
Manish <keshraram294@gmail.com>
2024-06-02 18:34:00 +0530

    shery-thon final version

---
+++ data.js
@@ -0,0 +1,51 @@
+const data = [
+    {
+        img:'./assets/img/domination.webp',
+        text:'domination',
+        x:5,
+        y:10,
+    },
+    {
+        img:'./assets/img/parrallex2.webp',
+        text:'second',
+        x:-2,
+        y:80,
+    },
+    {
+        img:'./assets/img/parrallex3.webp',
+        text:'third',
+        x:100,
+        y:20
+    },
+    {
+        img:'./assets/img/parrallex6.webp',
+        text:'friends',
+        x:50,
+        y:50
+    },
+    {
+        img:'./assets/img/parrallex5.webp',
+        text:'fifth',
+        x:100,
+        y:80,
+    },
+]
+
+const scrolImges = [
+    './assets/img/hanumaanJi.webp',
+    './assets/img/cynthia1.webp',
+    './assets/img/cynthia2.webp',
+    './assets/img/cynthia3.webp',
+    './assets/img/parrallex5.webp',
+    './assets/img/spiderman.webp',
+    './assets/img/domination.webp',
+    './assets/img/walls.webp',
+    './assets/img/saturn.webp',
+    './assets/img/porsche.webp',
+    './assets/img/money.webp',
+    './assets/img/img1.webp',
+    './assets/img/eagle.webp',
+    './assets/img/krishana.webp',
+]
+
+export {data , scrolImges}
